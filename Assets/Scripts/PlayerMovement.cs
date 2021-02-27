f7685c75371501cd3b653baeebca6d20d565e51d
Jack Coggins <voidawake90@gmail.com>
2021-02-27 14:21:01 +1100

    Comment out playerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,13 @@ public class PlayerMovement : MonoBehaviour
     PlayerControls controls = new PlayerControls();
 
     private float movement;
-    
+
     void Start()
     {
-        controls.PlayerControls.Jump.performed += ctx => Jump();
+    //     controls.PlayerControls.Jump.performed += ctx => Jump();
 
-        controls.PlayerControls.Movement.performed += ctx => movement = ctx.ReadValue<float>();
-        controls.PlayerControls.Movement.performed += ctx => movement = 0;
+    //     controls.PlayerControls.Movement.performed += ctx => movement = ctx.ReadValue<float>();
+    //     controls.PlayerControls.Movement.performed += ctx => movement = 0;
     }
 
     private void Update() {
@@ -22,21 +22,21 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Jump() {
-        
+
     }
 
     private void Move()
     {
-        
+
     }
 
-    private bool CheckCollisions(Vector2 direction) {
-        // Raycast in direction of movement
+    // private bool CheckCollisions(Vector2 direction) {
+    //     Raycast in direction of movement
 
-        RaycastHit hit;
+    //     RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity)) {
-            
-        }
-    }
+    //     if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity)) {
+
+    //     }
+    // }
 }
